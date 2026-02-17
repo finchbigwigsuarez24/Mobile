19c115fb2c433429b182a111a3fb5a42f081925c
Bigg Wigg <finchbigwig.suarez.24@usjr.edu.ph>
2026-02-17 18:56:02 +0800

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,11 @@ class MyApp extends StatelessWidget
       title: 'My Profile App', //title of the app
       debugShowCheckedModeBanner: false, //removes the debug banner
       theme: ThemeData.light(), //sets the theme of the app to light mode
-      //home: const ProfilePage(), //The first screen shown when the app opens
+      home: const ProfilePage(), //The first screen shown when the app opens
     );
   }
 }
 
-
-// Wala pa sugdi!!!!!
 class ProfilePage extends StatelessWidget
 {
   // ignore: use_super_parameters
@@ -35,14 +33,67 @@ class ProfilePage extends StatelessWidget
   {
     return Scaffold
     (
-      appBar: AppBar
+      appBar: AppBar(title: Text('My Profile')), //the top bar of the app with the title 'My Profile'
+
+      body: SingleChildScrollView //allows the content to be scrollable if it exceeds the screen height
       (
-        title: const Text('My Profile'), //title of the app bar
-        centerTitle: true, //centers the title in the app bar
-      ),
-      body: const Center
-      (
-        child: Text('Welcome to my profile!'), //welcome message in the center of the screen
+        padding: EdgeInsets.all(24),
+        child: Column
+        (
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:
+          [
+            SizedBox(height: 8),
+            CircleAvatar
+            (
+              radius: 100,
+              backgroundImage: NetworkImage('https://scontent.fceb1-2.fna.fbcdn.net/v/t39.30808-6/454263809_506873101750850_5347845284382279208_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=JSx3swejA6wQ7kNvwG9rPX2&_nc_oc=AdnJyNSAWLbAptTgGBS2Mu2P2nMM6S9EtZUCKWjYpv-DLYZkKg9TLn-d_PqyeZ7znfw&_nc_zt=23&_nc_ht=scontent.fceb1-2.fna&_nc_gid=xeXv7dv12n3cayXZlSGFJQ&oh=00_AfuajujFsZGltNae42EyQRfdGuuYDpFvVNlpBary7QVO6A&oe=699894D0'),
+            ),
+            SizedBox(height: 16),
+            Text('Finch Bigwig S. Suarez', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            SizedBox(height: 6),
+            Text('Student Developer', style: TextStyle(fontSize: 16, color: Colors.grey[700])),
+            SizedBox(height: 16),
+            Divider(),
+            Card
+            (
+              child: ListTile
+              (
+                leading: Icon(Icons.email),
+                title: Text('finchbigwig.suarez.24@usjr.edu.ph'),
+                subtitle: Text('Email'),
+              ),
+            ),
+            Card
+            (
+              child: ListTile
+              (
+                leading: Icon(Icons.phone, color: Colors.green),
+                title: Text('0943 331 1192'),
+                subtitle: Text('Number'),                
+              ),
+            ),
+            Card
+            (
+              child: ListTile
+              (
+                leading: Icon(Icons.cake),
+                title: Text('October 1, 2006'),
+                subtitle: Text('Birthday'),
+              ),
+            ),
+            Card
+            (
+              child: ListTile
+              (
+                leading: Icon(Icons.home),
+                title: Text('Laray, San Roque, Talisay City, Cebu'),
+                subtitle: Text('Address'),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
